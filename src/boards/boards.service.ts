7a493ef8de0c4c834dccc9aabb9348c1061652e2
rimo <munak.dev@gmail.com>
2023-11-02 01:06:29 +0900

    feat: 추가 게시물 생성하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
   }
 }
