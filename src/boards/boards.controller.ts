65dfffaacd5c33c64c478ec79da70ca0d6da9ea5
rimo <munak.dev@gmail.com>
2023-11-01 23:01:20 +0900

    feat :추가 보드 컨트롤러

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('boards')
+export class BoardsController {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { BoardsController } from './boards.controller';
 
-@Module({})
+@Module({
+  controllers: [BoardsController],
+})
 export class BoardsModule {}
