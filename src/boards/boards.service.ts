2f20a5c776702e775709061acf2f27559d6d7467
rimo <munak.dev@gmail.com>
2023-11-02 14:36:18 +0900

    feat: 없는 게시물을 조회할 경우의 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -24,7 +24,11 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+    return found;
   }
 
   deleteBoard(id: string): void {
