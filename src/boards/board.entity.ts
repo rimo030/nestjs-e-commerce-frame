319275951c1f3edf9279f91113abf2e20c7a7aff
rimo <munak.dev@gmail.com>
2023-11-02 18:18:38 +0900

    feat: Board Entitiy 생성

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './board.model';
+
+@Entity()
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  descrition: string;
+
+  @Column()
+  status: BoardStatus;
+}
