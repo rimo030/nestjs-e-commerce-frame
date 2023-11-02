b6428f4e8638d44dbc32efa6fb359ecaca348272
rimo <munak.dev@gmail.com>
2023-11-02 19:53:54 +0900

    chore: DB를 이용하기 위한 소스정리

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,5 +1,5 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { BoardStatus } from './board.model';
+import { BoardStatus } from './board.status.enum';
 
 @Entity()
 export class Board extends BaseEntity {
