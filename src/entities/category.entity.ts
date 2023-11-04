2d121e21048301c8ff1bbaa584ecd93bd3482c44
rimo <munak.dev@gmail.com>
2023-11-05 04:08:17 +0900

    feat: 로컬메모리 test 연습, category-prodect FK추가

--- src/entities/category.entity.ts
+++ src/entities/category.entity.ts
@@ -1,8 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { CommonEntity } from './common/common.entity';
+import { ProductEntity } from './product.entity';
 
 @Entity()
 export class CategoryEntity extends CommonEntity {
   @Column({ type: 'varchar', length: 32 })
   name!: string;
+
+  /**
+   * relations
+   */
+
+  @OneToMany(() => ProductEntity, (p) => p.categoryID)
+  products!: ProductEntity[];
 }
--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,8 +1,12 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { CommonEntity } from './common/common.entity';
+import { CategoryEntity } from './category.entity';
 
 @Entity()
 export class ProductEntity extends CommonEntity {
+  @Column()
+  categoryID!: number;
+
   @Column({ type: 'varchar', length: 32 })
   title!: string;
 
@@ -14,4 +18,12 @@ export class ProductEntity extends CommonEntity {
 
   @Column({ type: 'text' })
   company!: string;
+
+  /**
+   * relations
+   */
+
+  @ManyToOne(() => CategoryEntity, (c) => c.products)
+  @JoinColumn({ name: 'categoryID', referencedColumnName: 'id' })
+  category!: CategoryEntity;
 }
