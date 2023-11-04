53266ca6e09d0624974b66fb85475e5becce9fa6
rimo <munak.dev@gmail.com>
2023-11-04 17:49:00 +0900

    OrderProductInputOptionEntity 추가

--- src/entities/order-product-input-option.entity.ts
+++ src/entities/order-product-input-option.entity.ts
@@ -2,7 +2,7 @@ import { Entity, Column } from 'typeorm';
 import { CommonEntity } from './common/common.entity';
 
 @Entity()
-export class OrderProductInputOption extends CommonEntity {
+export class OrderProductInputOptionEntity extends CommonEntity {
   @Column({ type: 'varchar', length: '32' })
   name!: string;
 
