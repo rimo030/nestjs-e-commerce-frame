9640826eda1ed1655445eda2925e46284f7bad96
rimo <munak.dev@gmail.com>
2023-11-04 14:39:15 +0900

    feat: 코드 중복 제거를 위해 common entity 정의

--- src/entities/board.entity.ts
+++ src/entities/board.entity.ts
@@ -1,17 +1,15 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { BoardStatus } from '../types/enums/board-status.enum';
+import { CommonEntity } from './common/common.entity';
 
 @Entity()
-export class Board extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+export class Board extends CommonEntity {
+  @Column()
+  title!: string;
 
   @Column()
-  title: string;
+  descrition!: string;
 
   @Column()
-  descrition: string;
-
-  @Column()
-  status: BoardStatus;
+  status!: BoardStatus;
 }
