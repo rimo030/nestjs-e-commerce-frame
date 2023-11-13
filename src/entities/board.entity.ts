8cd13c3adf5ad7c4ccc912784ba468dcc6dd87a6
rimo <munak.dev@gmail.com>
2023-11-13 21:07:38 +0900

    feat: user-board FK 추가

--- src/entities/board.entity.ts
+++ src/entities/board.entity.ts
@@ -1,9 +1,13 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { BoardStatus } from '../types/enums/board-status.enum';
 import { CommonEntity } from './common/common.entity';
+import { UserEntity } from './user.entity';
 
 @Entity({ name: 'board' })
 export class Board extends CommonEntity {
+  @Column()
+  userId!: number;
+
   @Column({ type: 'varchar', length: 512 })
   title!: string;
 
@@ -12,4 +16,8 @@ export class Board extends CommonEntity {
 
   @Column({ name: 'status', type: 'varchar', length: 128 })
   status!: keyof typeof BoardStatus;
+
+  @ManyToOne(() => UserEntity, (u) => u.boards)
+  @JoinColumn({ referencedColumnName: 'id' })
+  user!: UserEntity;
 }
