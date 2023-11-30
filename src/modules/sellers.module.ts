9d5ff157332302744b0704c131a33bf721cc7ced
rimo <munak.dev@gmail.com>
2023-11-30 22:10:10 +0900

    chore: 레포지토리 import

--- src/modules/sellers.module.ts
+++ src/modules/sellers.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CustomTypeOrmModule } from 'src/configs/custom-typeorm.module';
 import { ProductsBundleRespository } from 'src/repositories/products.bundle.repository';
+import { ProductsOptionRespository } from 'src/repositories/products.option.repository';
 import { ProductsRespository } from 'src/repositories/products.repository';
+import { ProductsRequiredRespository } from 'src/repositories/products.required.option.repository';
 import { SellerController } from '../controllers/sellers.controller';
 import { SellerService } from '../services/sellers.service';
 
 @Module({
-  imports: [CustomTypeOrmModule.forCustomRepository([ProductsRespository, ProductsBundleRespository])],
+  imports: [
+    CustomTypeOrmModule.forCustomRepository([
+      ProductsRespository,
+      ProductsBundleRespository,
+      ProductsRequiredRespository,
+      ProductsOptionRespository,
+    ]),
+  ],
   controllers: [SellerController],
   providers: [SellerService],
 })
