6860778113f83bb3b1b7008585d1dc713ba64145
rimo <munak.dev@gmail.com>
2023-12-01 19:48:57 +0900

    feat : 카테고리 조회 서비스 구현

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CategoryEntity } from 'src/entities/category.entity';
+import { CategoryRepository } from 'src/repositories/category.repository';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  async getCategory(): Promise<CategoryEntity[]> {
+    return this.categoryRepository.find();
+  }
+}
