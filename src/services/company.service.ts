ce4ba5cf5474e8037b41f4cb5a43bbc44106d422
rimo <munak.dev@gmail.com>
2023-12-03 21:23:25 +0900

    fix : 레포지토리 주입 오타 수정

--- src/services/company.service.ts
+++ src/services/company.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CompanyEntity } from 'src/entities/company.entity';
 import { PaginationDto } from 'src/entities/dtos/pagination.dto';
 import { CompanyRepository } from 'src/repositories/company.repository';
 import { getOffset } from 'src/util/functions/get-offset.function';
-import { getTotalPage } from 'src/util/functions/get-total-page.function';
 
 @Injectable()
 export class CompanyService {
   constructor(
-    @InjectRepository(CompanyEntity)
+    @InjectRepository(CompanyRepository)
     private readonly companyRepository: CompanyRepository,
   ) {}
 
