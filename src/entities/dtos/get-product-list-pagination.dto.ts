00aedbd819ecfc9cac15ad6c2213a335132c3cc7
rimo <munak.dev@gmail.com>
2023-12-15 17:11:49 +0900

    style : 상품리스트 조회 인터페이스명 변경

---
+++ src/entities/dtos/get-product-list-pagination.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptionalNumber } from 'src/decorators/is-optional-number.decorator';
+import { IsOptionalString } from 'src/decorators/is-optional-string.decorator';
+import { PaginationDto } from './pagination.dto';
+
+export class GetProductPaginationDto extends PaginationDto {
+  /**
+   * @todo
+   * 검색어 길이 제한 결정
+   */
+  @ApiProperty({ description: '검색 키워드', required: false })
+  @IsOptionalString(1, 20)
+  search?: string | null;
+
+  @ApiProperty({ description: '카테고리 id', required: false })
+  @IsOptionalNumber()
+  categoryId?: number | null;
+
+  @ApiProperty({ description: '판매자 id', required: false })
+  @IsOptionalNumber()
+  sellerId?: number | null;
+}
--- src/util/functions/create-product-pagination-form.function.ts
+++ src/util/functions/create-product-pagination-form.function.ts
@@ -1,18 +1,18 @@
-import { GetProductPaginationDto } from 'src/entities/dtos/product-pagination.dto';
+import { GetProductPaginationDto } from 'src/entities/dtos/get-product-list-pagination.dto';
 import { ProductEntity } from 'src/entities/product.entity';
-import { GetProductResponse } from 'src/interfaces/get-product-response.interface';
+import { GetProductListResponse } from 'src/interfaces/get-product-list-response.interface';
 import { GetResponse } from 'src/interfaces/get-response.interface';
 import { getTotalPage } from './get-total-page.function';
 
 export function createProductPaginationForm(
   getResponse: GetResponse<ProductEntity>,
   getProductPagintionDto: GetProductPaginationDto,
-): GetProductResponse {
+): GetProductListResponse {
   const { list, count, take } = getResponse;
   const totalPage = getTotalPage(count, take);
 
   const lastProductId = list.at(list.length - 1)?.id;
-  const result: GetProductResponse = {
+  const result: GetProductListResponse = {
     data: { list, ...totalPage, lastProductId: lastProductId ?? null },
     meta: getProductPagintionDto,
   };
