4dd82d859a373accc95bbbe278d70417d3393f4a
rimo <munak.dev@gmail.com>
2023-12-19 22:42:05 +0900

    fix : get-product, get-product-option DTO 수정

--- src/entities/dtos/get-product-options.dto.ts
+++ src/entities/dtos/get-product-options.dto.ts
@@ -1,17 +1,10 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
-import { IsNotEmptyNumber } from 'src/decorators/is-not-empty-number.decorator';
-import { ProductRequiredOptionEntity } from '../product-required-option.entity';
-import { CreateProductOptionsDto } from './create-product-options.dto';
+import { PickType } from '@nestjs/swagger';
+import { ProductOptionEntity } from '../product-option.entity';
 
-export class GetProductOptionsDto
-  extends PickType(CreateProductOptionsDto, ['name', 'price', 'isSale'] as const)
-  implements Pick<ProductRequiredOptionEntity, 'id' | 'productId'>
-{
-  @ApiProperty({ description: '필수옵션 Id' })
-  @IsNotEmptyNumber('int')
-  id!: number;
-
-  @ApiProperty({ description: '상품 Id' })
-  @IsNotEmptyNumber('int')
-  productId!: number;
-}
+export class GetProductOptionDto extends PickType(ProductOptionEntity, [
+  'id',
+  'productId',
+  'name',
+  'price',
+  'isSale',
+] as const) {}
