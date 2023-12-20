363cc515e5ebd69a3b7bb1210e802c8747db36f1
rimo <munak.dev@gmail.com>
2023-12-20 17:01:18 +0900

    feat : 상품조회 API 레포지토리 로직 분리
    ProductRepository에 getProduct 추가

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,6 +1,28 @@
 import { Repository } from 'typeorm';
+import { GetProductDto } from 'src/entities/dtos/get-product.dto';
 import { ProductEntity } from 'src/entities/product.entity';
 import { CustomRepository } from '../configs/custom-typeorm.decorator';
 
 @CustomRepository(ProductEntity)
-export class ProductRepository extends Repository<ProductEntity> {}
+export class ProductRepository extends Repository<ProductEntity> {
+  async getProduct(id: number): Promise<GetProductDto | null> {
+    return await this.findOne({
+      select: {
+        id: true,
+        bundleId: true,
+        categoryId: true,
+        companyId: true,
+        isSale: true,
+        name: true,
+        description: true,
+        deliveryType: true,
+        deliveryFreeOver: true,
+        deliveryCharge: true,
+        img: true,
+      },
+      where: {
+        id,
+      },
+    });
+  }
+}
