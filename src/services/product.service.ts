30b5fb4083215e4318a3c3a38270fc46b3e3fd4b
rimo <munak.dev@gmail.com>
2023-12-20 17:12:42 +0900

    fix : 분리된 레포지토리 메소드를 사용하도록 변경

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -26,32 +26,12 @@ export class ProductService {
 
     @InjectRepository(ProductOptionRepository)
     private readonly productOptionRepository: ProductOptionRepository,
-
-    @InjectRepository(ProductInputOptionRepository)
-    private readonly productInputOptionRepository: ProductInputOptionRepository,
   ) {}
 
   async getProduct(id: number): Promise<CreateProductDto> {
-    const product = await this.productRepository.findOne({
-      select: {
-        id: true,
-        bundleId: true,
-        categoryId: true,
-        companyId: true,
-        isSale: true,
-        name: true,
-        description: true,
-        deliveryType: true,
-        deliveryFreeOver: true,
-        deliveryCharge: true,
-        img: true,
-      },
-      where: {
-        id,
-      },
-    });
+    const product = await this.productRepository.getProduct(id);
 
-    if (product === null) {
+    if (!product) {
       throw new NotFoundException(`Can't find product id : ${id}`);
     }
 
@@ -77,13 +57,7 @@ export class ProductService {
     const productIds = list.map((el) => el.id);
 
     if (productIds.length) {
-      const raws: { productId: number; minimumPrice: number }[] = await this.productRequiredOptionRepository
-        .createQueryBuilder('pro')
-        .select('pro.productId as productId')
-        .addSelect('MIN(pro.price) as minimumPrice')
-        .where('pro.productId IN (:...productIds)', { productIds })
-        .groupBy('pro.productId')
-        .getRawMany();
+      const raws = await this.productRequiredOptionRepository.getRequiredOptionsRaw(productIds);
 
       return {
         list: list.map((product) => {
@@ -107,56 +81,15 @@ export class ProductService {
     const { skip, take } = getOffset(paginationDto);
 
     if (isRequire) {
-      const [list, count] = await this.productRequiredOptionRepository.findAndCount({
-        select: {
-          id: true,
-          productId: true,
-          name: true,
-          price: true,
-          isSale: true,
-          productInputOptions: {
-            id: true,
-            productRequiredOptionId: true,
-            name: true,
-            value: true,
-            description: true,
-            isRequired: true,
-          },
-        },
-        order: {
-          id: 'ASC',
-        },
-        relations: { productInputOptions: true },
-        where: {
-          productId,
-          isSale: true,
-        },
+      const [list, count] = await this.productRequiredOptionRepository.getRequiredOptionJoinInputOptions(
+        productId,
         skip,
         take,
-      });
-
-      if (list.length) return { list, count, take };
-
-      throw new NotFoundException(`There is no required option for product ${productId}.`);
+      );
+      if (!list) new NotFoundException(`There is no required option for product ${productId}.`);
+      return { list, count, take };
     } else {
-      const [list, count] = await this.productOptionRepository.findAndCount({
-        select: {
-          id: true,
-          productId: true,
-          name: true,
-          price: true,
-          isSale: true,
-        },
-        order: {
-          id: 'ASC',
-        },
-        where: {
-          productId,
-          isSale: true,
-        },
-        skip,
-        take,
-      });
+      const [list, count] = await this.productOptionRepository.getProductOptions(productId, skip, take);
       return { list, count, take };
     }
   }
