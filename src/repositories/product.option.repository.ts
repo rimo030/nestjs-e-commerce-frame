a2071695079fd825730ab8e4111dd276e60a5d04
rimo <munak.dev@gmail.com>
2023-12-20 17:11:28 +0900

    feat : 상품선택옵션 조회 API 레포지토리 로직 분리
    ProductOptionRepository에 getProductOptions 추가

--- src/repositories/product.option.repository.ts
+++ src/repositories/product.option.repository.ts
@@ -1,6 +1,28 @@
 import { Repository } from 'typeorm';
+import { GetProductOptionDto } from 'src/entities/dtos/get-product-options.dto';
 import { ProductOptionEntity } from 'src/entities/product-option.entity';
 import { CustomRepository } from '../configs/custom-typeorm.decorator';
 
 @CustomRepository(ProductOptionEntity)
-export class ProductOptionRepository extends Repository<ProductOptionEntity> {}
+export class ProductOptionRepository extends Repository<ProductOptionEntity> {
+  async getProductOptions(productId: number, skip: number, take: number): Promise<[GetProductOptionDto[], number]> {
+    return await this.findAndCount({
+      select: {
+        id: true,
+        productId: true,
+        name: true,
+        price: true,
+        isSale: true,
+      },
+      order: {
+        id: 'ASC',
+      },
+      where: {
+        productId,
+        isSale: true,
+      },
+      skip,
+      take,
+    });
+  }
+}
