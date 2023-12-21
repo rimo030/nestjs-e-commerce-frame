d82207f6643ebbd60a7d2cb65a8ed19e0729440d
rimo <munak.dev@gmail.com>
2023-12-21 19:06:29 +0900

    feat : Seller API 서비스에서 상품 선택 옵션 추가와 관련된 레포지토리 코드 분리
    상품 선택 옵션만 다를뿐 앞선 커밋과 방향이 같음.
    ProductOptionRepository에 아래 methoed를 추가
    
    - createOption
    - getOption

--- src/repositories/product-option-repository.ts
+++ src/repositories/product-option-repository.ts
@@ -1,10 +1,31 @@
 import { Repository } from 'typeorm';
+import { CreateProductOptionsDto } from 'src/entities/dtos/create-product-options.dto';
 import { GetProductOptionDto } from 'src/entities/dtos/get-product-options.dto';
 import { ProductOptionEntity } from 'src/entities/product-option.entity';
 import { CustomRepository } from '../configs/custom-typeorm.decorator';
 
 @CustomRepository(ProductOptionEntity)
 export class ProductOptionRepository extends Repository<ProductOptionEntity> {
+  async createOption(productId: number, createProductOptionsDto: CreateProductOptionsDto): Promise<{ id: number }> {
+    const { id, ...other } = await this.save({ productId, ...createProductOptionsDto });
+    return { id };
+  }
+
+  async getOption(id: number): Promise<GetProductOptionDto | null> {
+    return await this.findOne({
+      select: {
+        id: true,
+        productId: true,
+        name: true,
+        price: true,
+        isSale: true,
+      },
+      where: {
+        id,
+      },
+    });
+  }
+
   async getProductOptions(productId: number, skip: number, take: number): Promise<[GetProductOptionDto[], number]> {
     return await this.findAndCount({
       select: {
