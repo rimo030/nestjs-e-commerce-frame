2085e98e6ca8e71a918d3983a1925e591942b3bb
rimo <munak.dev@gmail.com>
2023-12-21 19:14:24 +0900

    fix : Seller API 상품 옵션 등록 시 반환 객체 변경
    엔티티 반환에서 DTO 반환으로 수정
    
    ProductRequiredOptionEntity | ProductOptionEntity
    --> GetProductRequiredOptionDto | GetProductOptionDto

--- src/controllers/seller.controller.ts
+++ src/controllers/seller.controller.ts
@@ -5,6 +5,8 @@ import { UserId } from 'src/decorators/user-id.decorator';
 import { CreateProductBundleDto } from 'src/entities/dtos/create-product-bundle.dto';
 import { CreateProductOptionsDto } from 'src/entities/dtos/create-product-options.dto';
 import { CreateProductDto } from 'src/entities/dtos/create-product.dto';
+import { GetProductOptionDto } from 'src/entities/dtos/get-product-options.dto';
+import { GetProductRequiredOptionDto } from 'src/entities/dtos/get-product-required-option.dto';
 import { IsRequireOptionDto } from 'src/entities/dtos/is-require-options.dto';
 import { ProductOptionEntity } from 'src/entities/product-option.entity';
 import { ProductRequiredOptionEntity } from 'src/entities/product-required-option.entity';
@@ -42,7 +44,7 @@ export class SellerController {
     @Param('id', ParseIntPipe) productId: number,
     @Query() isRequireOptionDto: IsRequireOptionDto,
     @Body() createProductOptionsDto: CreateProductOptionsDto,
-  ): Promise<ProductRequiredOptionEntity | ProductOptionEntity> {
+  ): Promise<GetProductRequiredOptionDto | GetProductOptionDto> {
     return await this.sellerservice.createProductOptions(
       sellerId,
       productId,
