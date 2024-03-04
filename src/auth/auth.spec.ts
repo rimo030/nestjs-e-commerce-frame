709fa44d75516bf2e07ddd5745d9c85fe48803d8
rimo <munak.dev@gmail.com>
2024-03-04 23:39:06 +0900

    feat : 로그인 테스트 추가

--- src/auth/auth.spec.ts
+++ src/auth/auth.spec.ts
@@ -1,9 +1,11 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { CreateBuyerDto } from 'src/entities/dtos/create-buyer.dto';
 import { CreateSellerDto } from 'src/entities/dtos/create-seller.dto';
 import { BuyerRepository } from 'src/repositories/buyer.repository';
-import { SellerRepository } from '../repositories/seller.repository';
+import { SellerRepository } from 'src/repositories/seller.repository';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
@@ -12,8 +14,10 @@ describe('Controller', () => {
   let service: AuthService;
   let buyerRepository: BuyerRepository;
   let sellerRepository: SellerRepository;
+  let jwtService: JwtService;
+  let config: ConfigService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -22,6 +26,8 @@ describe('Controller', () => {
     service = module.get<AuthService>(AuthService);
     buyerRepository = module.get<BuyerRepository>(BuyerRepository);
     sellerRepository = module.get<SellerRepository>(SellerRepository);
+    jwtService = module.get<JwtService>(JwtService);
+    config = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
@@ -29,40 +35,66 @@ describe('Controller', () => {
     expect(service).toBeDefined();
     expect(buyerRepository).toBeDefined();
     expect(sellerRepository).toBeDefined();
+    expect(jwtService).toBeDefined();
+    expect(config).toBeDefined();
   });
 
   describe('Buyer 테스트', () => {
+    const testBuyer: CreateBuyerDto = {
+      email: 'myemail@gmail.com',
+      password: 'mypassword1!',
+      name: 'myname',
+      gender: 1,
+      age: 20,
+      phone: '01012341234',
+    };
+
     it('회원가입이 되었다면 데이터 베이스에서 이메일을 조회할 수 있어야 한다.', async () => {
-      const testBuyer: CreateBuyerDto = {
-        email: 'myemail@gmail.com',
-        password: 'mypassword1!',
-        name: 'myname',
-        gender: 1,
-        age: 20,
-        phone: '01012341234',
-      };
-      await service.buyerSignUp(testBuyer);
+      await controller.buyerSignUp(testBuyer);
       const savedBuyer = await buyerRepository.findByEmail(testBuyer.email);
       await buyerRepository.deleteById(savedBuyer.id);
 
       expect(savedBuyer.email).toBe(testBuyer.email);
     });
+
+    it('로그인이 되었다면, buyer의 id를 담은 access Token이 발급되어야 한다.', async () => {
+      await controller.buyerSignUp(testBuyer);
+      const { id, ...rest } = await buyerRepository.findByEmail(testBuyer.email);
+
+      const { accessToken } = await controller.buyerSignIn(testBuyer, { user: { id } });
+      const decode = jwtService.decode(accessToken!);
+      await buyerRepository.deleteById(id);
+
+      expect(decode.id).toBe(id);
+    });
   });
 
   describe('Seller 테스트', () => {
+    const testSeller: CreateSellerDto = {
+      email: 'myemail@gmail.com',
+      password: 'mypassword1!',
+      name: 'myname',
+      phone: '01012341234',
+      businessNumber: '12341234',
+    };
+
     it('회원가입이 되었다면 데이터 베이스에서 이메일을 조회할 수 있어야 한다.', async () => {
-      const testSeller: CreateSellerDto = {
-        email: 'myemail@gmail.com',
-        password: 'mypassword1!',
-        name: 'myname',
-        phone: '01012341234',
-        businessNumber: '12341234',
-      };
-      await service.sellerSignUp(testSeller);
+      await controller.sellerSignUp(testSeller);
       const savedSeller = await sellerRepository.findByEmail(testSeller.email);
       await sellerRepository.deleteById(savedSeller.id);
 
       expect(savedSeller.email).toBe(testSeller.email);
+    });
+
+    it('로그인이 되었다면, seller id를 담은 access Token이 발급되어야 한다.', async () => {
+      await controller.sellerSignUp(testSeller);
+      const { id, ...rest } = await sellerRepository.findByEmail(testSeller.email);
+
+      const { accessToken } = await controller.sellerSignIn(testSeller, { user: { id } });
+      const decode = jwtService.decode(accessToken!);
+      await sellerRepository.deleteById(id);
+
+      expect(decode.id).toBe(id);
     });
   });
 });
