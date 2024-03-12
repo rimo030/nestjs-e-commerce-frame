1ff99bdd2aec62a2675adf128bce02dd0e3f26fd
rimo <munak.dev@gmail.com>
2024-03-13 03:48:46 +0900

    feat : 장바구니 추가 API 컨트롤러 정의

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { BuyerJwtAuthGuard } from 'src/auth/guards/buyer-jwt.guard';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { CartDto } from 'src/entities/dtos/cart.dto';
+import { CreateCartDto } from 'src/entities/dtos/create-cart.dto';
+import { CartService } from 'src/services/cart.service';
 
 @Controller('cart')
-export class CartController {}
+@ApiBearerAuth('token')
+@ApiTags('Cart API')
+@UseGuards(BuyerJwtAuthGuard)
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @HttpCode(201)
+  @Post()
+  @ApiOperation({ summary: '장바구니 추가 API', description: '모든 사용자는 장바구니에 상품을 담을 수 있다.' })
+  async addCart(@UserId() buyerId: number, @Body() createCartDto: CreateCartDto): Promise<{ data: CartDto }> {
+    const cart = await this.cartService.addCart(buyerId, createCartDto);
+    return { data: cart };
+  }
+}
