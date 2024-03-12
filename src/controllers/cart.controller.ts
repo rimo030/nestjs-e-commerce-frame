5f1f8a76cdb6297fd71598841f04166328b185c1
rimo <munak.dev@gmail.com>
2024-03-13 04:26:10 +0900

    fix : dto에 값이 넘어오지 않는 것을 확인...

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -3,7 +3,6 @@ import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { BuyerJwtAuthGuard } from 'src/auth/guards/buyer-jwt.guard';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { CartDto } from 'src/entities/dtos/cart.dto';
-import { CreateCartDto } from 'src/entities/dtos/create-cart.dto';
 import { CartService } from 'src/services/cart.service';
 
 @Controller('cart')
@@ -16,7 +15,7 @@ export class CartController {
   @HttpCode(201)
   @Post()
   @ApiOperation({ summary: '장바구니 추가 API', description: '모든 사용자는 장바구니에 상품을 담을 수 있다.' })
-  async addCart(@UserId() buyerId: number, @Body() createCartDto: CreateCartDto): Promise<{ data: CartDto }> {
+  async addCart(@UserId() buyerId: number, @Body() createCartDto: any): Promise<{ data: CartDto }> {
     const cart = await this.cartService.addCart(buyerId, createCartDto);
     return { data: cart };
   }
