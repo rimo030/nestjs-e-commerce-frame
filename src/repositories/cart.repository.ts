ab1eb8002e200cdb680cb17a62f682b7cbb84958
rimo <munak.dev@gmail.com>
2024-03-13 03:56:28 +0900

    feat : cart 레포지토리 함수 작성
    - 장바구니 추가에 이용됩니다.
    - 두 함수 모두 인자로 buyer(구매자)의 id와 product(상품)을 받습니다.
    - findCart() : 카드의 정보 뿐만 아니라 필수/선택 옵션도 조인해서 가져옵니다.

--- src/repositories/cart.repository.ts
+++ src/repositories/cart.repository.ts
@@ -3,4 +3,17 @@ import { CartEntity } from 'src/entities/cart.entity';
 import { CustomRepository } from '../configs/custom-typeorm.decorator';
 
 @CustomRepository(CartEntity)
-export class CartRepository extends Repository<CartEntity> {}
+export class CartRepository extends Repository<CartEntity> {
+  async saveCart(buyerId: number, productId: number): Promise<CartEntity> {
+    return await this.save({ buyerId, productId });
+  }
+  async findCart(buyerId: number, productId: number): Promise<CartEntity | null> {
+    const cart = await this.createQueryBuilder('cart')
+      .leftJoinAndSelect('cart.cartRequiredOptions', 'cartRequiredOption')
+      .leftJoinAndSelect('cart.cartOptions', 'cartOption')
+      .where('cart.buyerId = :buyerId', { buyerId })
+      .andWhere('cart.productId = :productId', { productId })
+      .getOne();
+    return cart;
+  }
+}
