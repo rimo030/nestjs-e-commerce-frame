9dc286b7c7651ba68da69e0fcc7ca4b923291e18
rimo <munak.dev@gmail.com>
2024-03-13 22:07:28 +0900

    chore : 장바구니 생성 컨트롤러 반환값 수정

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -4,6 +4,7 @@ import { BuyerJwtAuthGuard } from 'src/auth/guards/buyer-jwt.guard';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { CartDto } from 'src/entities/dtos/cart.dto';
 import { CreateCartDto } from 'src/entities/dtos/create-cart.dto';
+import { UpdateCartDto } from 'src/entities/dtos/update-cart.dto';
 import { CartService } from 'src/services/cart.service';
 
 @Controller('cart')
@@ -16,7 +17,12 @@ export class CartController {
   @HttpCode(201)
   @Post()
   @ApiOperation({ summary: '장바구니 추가 API', description: '모든 사용자는 장바구니에 상품을 담을 수 있다.' })
-  async addCart(@UserId() buyerId: number, @Body() createCartDto: CreateCartDto): Promise<{ data: CartDto }> {
+  async addCart(
+    @UserId() buyerId: number,
+    @Body() createCartDto: CreateCartDto,
+  ): Promise<{
+    data: CartDto | UpdateCartDto;
+  }> {
     const cart = await this.cartService.addCart(buyerId, createCartDto);
     return { data: cart };
   }
