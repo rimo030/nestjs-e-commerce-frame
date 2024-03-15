8981f3cac481f5abb4acadebaf2a98b47ac04960
rimo <munak.dev@gmail.com>
2024-03-15 19:11:33 +0900

    chore : 데코레이터 수정
    - 장바구니 생성시 count에 음수가 들어올 수 있다.
    - 정수형 class-validate 데코레이터 결합 시 값의 최소/최대를 검증하도록 추가

--- src/decorators/is-optional-number.decorator.ts
+++ src/decorators/is-optional-number.decorator.ts
@@ -1,11 +1,13 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsNumber, IsOptional } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, Max, Min } from 'class-validator';
 import { applyDecorators } from '@nestjs/common';
 
-export function IsOptionalNumber(type: 'number' | 'int' = 'number') {
+export function IsOptionalNumber(type: 'number' | 'int' = 'number', option?: { min?: number; max?: number }) {
   return applyDecorators(
     IsOptional(),
     type === 'int' ? IsInt() : IsNumber(),
+    ...(typeof option?.min === 'number' ? [Min(option.min)] : []),
+    ...(typeof option?.max === 'number' ? [Max(option.max)] : []),
     Type(() => Number),
   );
 }
