41e66427c6abde9c31b4bcfcd540b3d3ea464ce0
rimo <munak.dev@gmail.com>
2024-03-18 23:18:51 +0900

    feat : exception에 대한 로그 추가

--- src/filter/http-exception.filter.ts
+++ src/filter/http-exception.filter.ts
@@ -1,17 +1,23 @@
 import { Request, Response } from 'express';
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
+    this.logger.error(
+      `${request.method} ${request.url} - ${status} - ${JSON.stringify(exception.getResponse(), null, 2)}`,
+    );
+
     response.status(status).json({
       statusCode: status,
-      message: exception.message,
+      message: exception.getResponse(),
       path: request.url,
       timestamp: new Date().toISOString(),
     });
