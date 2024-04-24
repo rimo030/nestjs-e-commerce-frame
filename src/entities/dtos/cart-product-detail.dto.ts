95b2be4bab9e4569b4cbf73e20dc6e65c7001e45
rimo <munak.dev@gmail.com>
2024-04-24 16:14:08 +0900

    fix : 장바구니 조회 dto 수정
    - 장바구니 옵션의 id가 아닌, 상품 옵션의 id가 반환되던 오류 해결

--- src/entities/dtos/cart-product-detail.dto.ts
+++ src/entities/dtos/cart-product-detail.dto.ts
@@ -1,8 +1,8 @@
+import { CartOptionEntity } from '../cart-option.entity';
+import { CartRequiredOptionEntity } from '../cart-required-option.entity';
 import { CartEntity } from '../cart.entity';
-import { CartOptionDto } from './cart-option.dto';
-import { CartRequiredOptionDto } from './cart-required-option.dto';
-import { ProductOptionDto } from './product-option.dto';
-import { ProductRequiredOptionDto } from './product-required-option.dto';
+import { ProductOptionEntity } from '../product-option.entity';
+import { ProductRequiredOptionEntity } from '../product-required-option.entity';
 import { ProductDto } from './product.dto';
 
 export class CartProductDetailDto {
@@ -10,8 +10,10 @@ export class CartProductDetailDto {
   buyerId!: number;
   productId!: number;
   product: ProductDto;
-  cartRequiredOptions!: (CartRequiredOptionDto & ProductRequiredOptionDto)[];
-  cartOptions!: (CartOptionDto & ProductOptionDto)[];
+  cartRequiredOptions!: (Pick<CartRequiredOptionEntity, 'id' | 'cartId' | 'productRequiredOptionId' | 'count'> &
+    Pick<ProductRequiredOptionEntity, 'productId' | 'price' | 'name' | 'isSale'>)[];
+  cartOptions!: (Pick<CartOptionEntity, 'id' | 'cartId' | 'productOptionId' | 'count'> &
+    Pick<ProductOptionEntity, 'productId' | 'price' | 'name' | 'isSale'>)[];
 
   constructor(cart: CartEntity) {
     this.id = cart.id;
@@ -19,10 +21,28 @@ export class CartProductDetailDto {
     this.productId = cart.productId;
     this.product = new ProductDto(cart.product);
     this.cartRequiredOptions = cart.cartRequiredOptions.map((ro) => {
-      return { ...new CartRequiredOptionDto(ro), ...new ProductRequiredOptionDto(ro.productRequiredOption) };
+      return {
+        id: ro.id,
+        cartId: ro.cartId,
+        productId: ro.productRequiredOption.productId,
+        productRequiredOptionId: ro.productRequiredOptionId,
+        count: ro.count,
+        price: ro.productRequiredOption.price,
+        name: ro.productRequiredOption.name,
+        isSale: ro.productRequiredOption.isSale,
+      };
     });
     this.cartOptions = cart.cartOptions.map((o) => {
-      return { ...new CartOptionDto(o), ...new ProductOptionDto(o.productOption) };
+      return {
+        id: o.id,
+        cartId: o.cartId,
+        productId: o.productOption.productId,
+        productOptionId: o.productOptionId,
+        count: o.count,
+        price: o.productOption.price,
+        name: o.productOption.name,
+        isSale: o.productOption.isSale,
+      };
     });
   }
 }
