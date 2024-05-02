6400349b3385a6bbedcc79669185a13c513416a5
rimo <munak.dev@gmail.com>
2024-05-02 16:55:43 +0900

    refacotr : 상품 묶음 등록 DTO 타입 수정
    - 프리즈마 ChargeStandard enum 생성
    - 프리즈마 모델 타입으로 변경
    - chargeStandard 타입 문자열 리터럴로 재작성
    - 스웨거 및 cv 수정

--- src/entities/dtos/create-product-bundle.dto.ts
+++ src/entities/dtos/create-product-bundle.dto.ts
@@ -1,21 +1,21 @@
+import { ProductBundle } from '@prisma/client';
 import { IsEnum } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmptyString } from 'src/decorators/is-not-empty-string.decorator';
-import { ProductBundleEntity } from 'src/entities/product-bundle.entity';
-import { chargeStandard } from 'src/types/enums/charge-standard.enum';
+import { chargeStandard } from 'src/types/charge-standard.type';
 
-export class CreateProductBundleDto implements Pick<ProductBundleEntity, 'name' | 'chargeStandard'> {
+export class CreateProductBundleDto implements Pick<ProductBundle, 'name' | 'chargeStandard'> {
   @ApiProperty({ type: String, description: '묶음 배송 이름', required: true, example: 'Test Bundle name' })
   @IsNotEmptyString(1, 128)
   name!: string;
 
   @ApiProperty({
     type: 'enum',
-    enum: chargeStandard,
+    enum: ['MIN', 'MAX'],
     description: '묶음 배송 배송비 기준 값 ("MIN", "MAX" 허용)',
     required: true,
     example: 'MIN',
   })
-  @IsEnum(chargeStandard)
-  chargeStandard!: keyof typeof chargeStandard;
+  @IsEnum(['MIN', 'MAX'])
+  chargeStandard!: chargeStandard;
 }
