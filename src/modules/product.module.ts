1065f6de80897ab7656658428a82695f83c86907
rimo <munak.dev@gmail.com>
2024-05-14 22:43:08 +0900

    chore : 레포지토리 import 제거

--- src/modules/product.module.ts
+++ src/modules/product.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CustomTypeOrmModule } from 'src/configs/custom-typeorm.module';
-import { ProductInputOptionRepository } from 'src/repositories/product-input-option.repository';
-import { ProductOptionRepository } from 'src/repositories/product-option-repository';
-import { ProductRequiredOptionRepository } from 'src/repositories/product-required-option.repository';
-import { ProductRepository } from 'src/repositories/product.repository';
 import { ProductController } from '../controllers/product.controller';
 import { ProductService } from '../services/product.service';
 
 @Module({
-  imports: [
-    CustomTypeOrmModule.forCustomRepository([
-      ProductRepository,
-      ProductRequiredOptionRepository,
-      ProductOptionRepository,
-      ProductInputOptionRepository,
-    ]),
-  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
