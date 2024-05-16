c1208825a5c235491346017b292af13b7dc2e95d
rimo <munak.dev@gmail.com>
2024-05-16 17:01:08 +0900

    fix : 엔티티 정의에서 프리즈마 의존성 삭제

--- src/entities/dtos/product-option.dto.ts
+++ src/entities/dtos/product-option.dto.ts
@@ -1,3 +1,7 @@
-import { ProductOption } from '@prisma/client';
-
-export interface ProductOptionDto extends Pick<ProductOption, 'id' | 'productId' | 'name' | 'price' | 'isSale'> {}
+export interface ProductOptionDto {
+  id: number;
+  productId: number;
+  name: string;
+  price: number;
+  isSale: boolean;
+}
