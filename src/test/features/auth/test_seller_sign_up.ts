47e7ad9dee1355b88de4aa025c14a5662a9e9c11
kakasoo <kscodebase@gmail.com>
2024-05-27 22:14:45 +0900

    test : `test_seller_sign_up` 함수를 진행 시 판매자의 모든 정보를 랜덤으로 생성한다

--- src/test/features/auth/test_seller_sign_up.ts
+++ src/test/features/auth/test_seller_sign_up.ts
@@ -1,9 +1,11 @@
+import { v4 } from 'uuid';
 import { AuthController } from 'src/auth/auth.controller';
 import { CreateSellerDto } from 'src/dtos/create-seller.dto';
 
 /**
  * 판매자의 회원 가입을 테스트하는 함수이다.
  * e2e로 작성되었으며, 프론트에서 호출한 것과 동일한 과정을 수행한다.
+ * 어떠한 옵션도 받지 않으며, 판매자에 대한 정보는 전부 랜덤으로 생성된다.
  *
  *  endpoint는 `POST auth/signup-seller` 에 해당한다.
  *
@@ -14,11 +16,11 @@ export async function test_seller_sign_up(): Promise<ReturnType<AuthController['
     headers: {},
     method: 'POST',
     body: JSON.stringify({
-      businessNumber: '',
-      email: '',
-      name: '',
-      password: '',
-      phone: '',
+      email: `${v4().slice(0, 100)}@gmail.com`,
+      password: v4().slice(0, 20),
+      name: v4().slice(0, 10),
+      phone: v4().slice(0, 10),
+      businessNumber: v4().slice(0, 100),
     } satisfies CreateSellerDto),
   });
 
