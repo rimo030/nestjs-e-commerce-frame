c8b4097c942cae7a2891fe67202dcdde87ea54cb
kakasoo <kscodebase@gmail.com>
2024-05-27 21:52:24 +0900

    test : `test_seller_create_product` 작성
    - 판매자가 자신의 토큰을 이용할 수 있도록 로그인하는 상황을 가정해야 한다.
    - 판매자가 로그인하는 상황까지 가정해야 한다.

---
+++ src/test/features/sellers/test_seller_create_product.ts
@@ -0,0 +1,44 @@
+import { v4 } from 'uuid';
+import { SellerController } from 'src/controllers/seller.controller';
+import { CreateProductDto } from 'src/dtos/create-product.dto';
+
+/**
+ * 판매자가 직접 상품을 생성하는 상황을 가정한다.
+ * 리턴되는 상품은 Mock data로, 아무런 의미도 가지고 있지 않다.
+ * e2e 테스트를 위해 `fetch` 함수로 직접 테스트를 한다.
+ *
+ * @todo 추후 더 세밀한 테스트를 위해 option을 통해 상품을 세밀하게 수정할 수 있도록 한다.
+ *
+ * endpoint는 `POST seller/product` 에 해당한다.
+ *
+ * @param options 상품을 생성하기 위해 필요한 정보들
+ * @returns
+ */
+export async function test_seller_create_product(options: {
+  bundleId: number;
+  categoryId: number;
+  companyId: number;
+}): Promise<ReturnType<SellerController['createProduct']>> {
+  const { bundleId, categoryId, companyId } = options;
+  const response = await fetch('http://localhost/3000', {
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      bundleId: bundleId,
+      categoryId: categoryId,
+      companyId: companyId,
+      isSale: true,
+      name: v4().slice(0, 10),
+      description: v4().slice(0, 10),
+      deliveryType: 'FREE',
+      deliveryFreeOver: null,
+      deliveryCharge: 0,
+      img: v4().slice(0, 10),
+    } satisfies CreateProductDto),
+  });
+
+  const data: Awaited<ReturnType<SellerController['createProduct']>> = await response.json();
+  return data;
+}
