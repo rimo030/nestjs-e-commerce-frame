a08d3fb92e7655cf17322dce898ac8d4432017df
rimo <munak.dev@gmail.com>
2024-05-30 18:36:11 +0900

    feat : 회사 다수 생성 API 추가

--- src/controllers/company.controller.ts
+++ src/controllers/company.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, HttpCode, Post, Query, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SellerJwtAuthGuard } from 'src/auth/guards/seller-jwt.guard';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { CompanyDto } from 'src/dtos/company.dto';
@@ -15,6 +15,20 @@ import { createPaginationResponseDto } from 'src/util/functions/pagination-util.
 @UseGuards(SellerJwtAuthGuard)
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
+
+  @HttpCode(201)
+  @Post('bulk')
+  @ApiBody({ type: [CreateCompanyDto] })
+  @ApiOperation({ summary: '회사 다수 등록 API', description: '회사를 여러개 등록할 수 있다.' })
+  async createCompanies(
+    @UserId() sellerId: number,
+    @Body() createCompanyDtos: CreateCompanyDto[],
+  ): Promise<{
+    data: CompanyDto[];
+  }> {
+    const companies = await this.companyService.createCompanies(sellerId, createCompanyDtos);
+    return { data: companies };
+  }
 
   @HttpCode(201)
   @Post()
