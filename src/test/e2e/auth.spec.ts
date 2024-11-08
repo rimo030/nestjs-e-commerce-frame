cec073f0c0a2310c642be031fb68c43faf5f6c70
최주연 <rimo@gang-gyeongsuui-noteubug.local>
2024-11-09 00:39:00 +0900

    feat: buyer 회원가입 및 로그인 e2e 테스트 작성

--- src/test/e2e/auth.spec.ts
+++ src/test/e2e/auth.spec.ts
@@ -5,6 +5,8 @@ import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { test_seller_sign_in } from '../features/auth/test_seller_sign_in';
 import { test_seller_sign_up } from '../features/auth/test_seller_sign_up';
+import { test_buyer_sign_in } from '../features/auth/test_buyer_sign_in';
+import { test_buyer_sign_up } from '../features/auth/test_buyer_sign_up';
 
 describe('Controller', () => {
   const PORT = randomInt(20000, 50000);
@@ -24,6 +26,36 @@ describe('Controller', () => {
   afterAll(async () => {
     await server.close();
   });
+
+  describe('Buyer 테스트', () => {
+    it('구매자 회원 가입에 성공해야 한다.', async () => {
+      const response = await test_buyer_sign_up(PORT);
+
+      expect(response.data.id).toBeDefined();
+      expect(response.data.accessToken).toBeDefined();
+      expect(response.data.refreshToken).toBeDefined();
+    });
+
+    it(`구매자 로그인에 성공해야 한다. 회원가입이 실패할 경우, 함께 실패한다.`, async () => {
+      const response = await test_buyer_sign_in(PORT, {
+        email: `${v4()}@gmail.com`,
+        password: v4().slice(0, 20),
+      });
+      expect(response.data).toBeDefined();
+    });
+
+
+    it(`구매자 로그인에 성공한 경우 AccessToken, RefreshToken이 발급되어야 한다.`, async () => {
+      const response = await test_buyer_sign_in(PORT, {
+        email: `${v4()}@gmail.com`,
+        password: v4().slice(0, 20),
+      });
+      expect(response.data.id).toBeDefined();
+      expect(response.data.accessToken).toBeDefined();
+      expect(response.data.refreshToken).toBeDefined();
+    });
+  });
+
 
   describe('Seller 테스트', () => {
     it('판매자 회원 가입에 성공해야 한다.', async () => {
