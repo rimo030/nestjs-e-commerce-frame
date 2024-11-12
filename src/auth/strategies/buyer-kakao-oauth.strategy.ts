338428546f04307d15429133c098d32b36df3346
rimo <munak.dev@gmail.com>
2024-11-12 23:46:01 +0900

    buyer 카카오 oauth strategy validate  반환 인터페이스 정의 추가

--- src/auth/strategies/buyer-kakao-oauth.strategy.ts
+++ src/auth/strategies/buyer-kakao-oauth.strategy.ts
@@ -2,6 +2,7 @@ import { Profile, Strategy } from 'passport-kakao';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { BuyerKakaoCredentialsRequest } from 'src/interfaces/buyer-kakao-login.request.interface';
 
 @Injectable()
 export class BuyerKakaoStrategy extends PassportStrategy(Strategy, 'buyer-kakao') {
@@ -12,19 +13,10 @@ export class BuyerKakaoStrategy extends PassportStrategy(Strategy, 'buyer-kakao'
     });
   }
 
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: Profile,
-  ): Promise<{
-    kakaoId: string | undefined;
-    name: string | undefined;
-    accessToken: string;
-  }> {
+  async validate(accessToken: string, refreshToken: string, profile: Profile): Promise<BuyerKakaoCredentialsRequest> {
     const { id, username } = profile;
-
     return {
-      kakaoId: id,
+      id: `${id}`,
       name: username,
       accessToken,
     };
---
+++ src/interfaces/buyer-kakao-login.request.interface.ts
@@ -0,0 +1,5 @@
+export interface BuyerKakaoCredentialsRequest {
+  id: string;
+  name?: string | null;
+  accessToken: string;
+}
